da4d32e229c32e906e0633793c5b2ebea1b5b713
OracleRep <smstep705@gmail.com>
2026-02-08 18:30:44 +0200

    bucket name and cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,13 +27,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "kopsbucketstate2701"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
 }
-##
-##
-##
+
