2ddfafeacc7961c210d89902d21567fcbfe42a6d
OracleRep <smstep705@gmail.com>
2026-02-25 13:21:05 +0200

    fix: terraform fmt

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,7 +1,7 @@
-check for fmt module "eks" {
+check "for" "fmt" "module" "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
-  
+
 
   cluster_name    = local.cluster_name
   cluster_version = "1.33"
